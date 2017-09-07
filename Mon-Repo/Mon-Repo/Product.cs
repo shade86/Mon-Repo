32c680b5c636801b593700fdaaf4e9af172160cc
shade86 <31279365+shade86@users.noreply.github.com>
2017-09-07 21:11:14 +0200

    ProductList window
    ProductForm még kell, gombok + eseménykezelés

---
+++ Mon-Repo/Mon-Repo/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mon_Repo
+{
+    public class Product
+    {
+        string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChange();
+            }
+        }
+        int _price;
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                OnPropertyChange();
+            }
+        }
+    }
+}
